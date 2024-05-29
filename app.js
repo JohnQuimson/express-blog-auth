35a4b95cdf2f47762d87cfb58abc1c63e0c2f940
JohnQuimson <john.quimson777@gmail.com>
2024-05-29 14:57:48 +0200

    feat: /login

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const users = require('./db/users.json');
+const { generateToken } = require('./middlewares/jwt.js');
+
+// body parser json
+app.use(express.json());
+// body parser per application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+  if (!user) {
+    return res.status(404).send(`Credenziali errate`);
+  }
+  const token = generateToken(user);
+  res.send(token);
+});
 
 const postsRouter = require('./routers/posts.js');
 const routersLogger = require('./middlewares/routersLogger.js');
 const errorsFormatter = require('./middlewares/errorsFormatter.js');
 const routesNotFound = require('./middlewares/routesNotFound.js');
-
-// body parser per application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('./public'));
 
@@ -16,7 +32,7 @@ app.use(routersLogger);
 
 // routers
 app.get('/', (req, res) => {
-  res.send(`<h1>Posts</h1>`);
+  res.send(`<h1>Benvenuto nel post con dati sensibili!</h1>`);
 });
 
 app.use('/posts', postsRouter);
